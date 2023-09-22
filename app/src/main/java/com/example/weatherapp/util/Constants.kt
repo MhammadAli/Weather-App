e8a80aa0370be71db7ddc05248aeb68433e1dd47
MhammadAli <Mohammed180726@feng.bu.edu.eg>
2023-09-22 19:10:08 +0300

    Initial commit

---
+++ app/src/main/java/com/example/weatherapp/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.weatherapp.util
+
+object Constants {
+
+    const val BASE_URL = "https://api.weatherbit.io/v2.0/current"
+    const val API_KEY = "646e761c3c634339a0961390cd9db925"
+}
